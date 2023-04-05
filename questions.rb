5e3e26db37986bfa1b47370f9c1700010383b01c
carveyh <carvey.hor@gmail.com>
2023-04-05 14:52:02 -0400

    Begin adding Query methods up to User#authored_questions

--- questions.rb
+++ questions.rb
@@ -11,6 +11,43 @@ class QuestionsDatabase < SQLite3::Database
 	end 
 end
 
+class User
+
+	attr_accessor :id, :fname, :lname
+
+	def self.find_by_id(id)
+		data = QuestionsDatabase.instance.execute(<<-SQL, id)
+		SELECT 
+	    *
+		FROM users
+        WHERE id = ?  
+		SQL
+		data.map { |datum| Question.new(datum) }
+	end
+
+	def self.find_by_name(fname, lname)
+		data = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+		SELECT 
+	    *
+		FROM 
+			users
+		WHERE 
+			fname = ? AND lname = ?
+		SQL
+		data.map { |datum| Question.new(datum) }
+	end
+
+	def initialize(options)
+        @id = options['id']
+		@fname = options['fname']
+		@lname = options['lname']
+	end
+
+	def authored_questions
+		Question.find_by_author_id(self.id)
+	end
+end
+
 class Question
 
 	attr_accessor :id, :title, :body, :user_id
@@ -21,6 +58,18 @@ class Question
 	    *
 		FROM questions
         WHERE id = ?  
+		SQL
+		data.map { |datum| Question.new(datum) }
+	end
+
+	def self.find_by_author_id(author_id)
+		data = QuestionsDatabase.instance.execute(<<-SQL, author_id) 
+			SELECT
+				*
+			FROM
+				questions
+			WHERE
+				user_id = ?
 		SQL
 		data.map { |datum| Question.new(datum) }
 	end
@@ -43,6 +92,30 @@ class Reply
 	    *
 		FROM replies
         WHERE id = ?  
+		SQL
+		data.map { |datum| Question.new(datum) }
+	end
+
+	def self.find_by_user_id(user_id)
+		data = QuestionsDatabase.instance.execute(<<-SQL, user_id) 
+			SELECT
+				*
+			FROM
+				replies
+			WHERE
+				user_id = ?
+		SQL
+		data.map { |datum| Question.new(datum) }
+	end
+
+	def self.find_by_question_id(question_id)
+		data = QuestionsDatabase.instance.execute(<<-SQL, question_id) 
+			SELECT
+				*
+			FROM
+				replies
+			WHERE
+				question_id = ?
 		SQL
 		data.map { |datum| Question.new(datum) }
 	end
@@ -101,23 +174,3 @@ class QuestionFollow
 
 end
 
-class User
-
-	attr_accessor :id, :fname, :lname
-
-	def self.find_by_id(id)
-		data = QuestionsDatabase.instance.execute(<<-SQL, id)
-		SELECT 
-	    *
-		FROM users
-        WHERE id = ?  
-		SQL
-		data.map { |datum| Question.new(datum) }
-	end
-
-	def initialize(options)
-        @id = options['id']
-		@fname = options['fname']
-		@lname = options['lname']
-	end
-end
